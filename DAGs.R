825624130c7e1dcdef8fe9b95c2ab87b8dbc3af5
Nathan A Clarke <n.clarke1986@gmail.com>
2019-11-25 07:05:57 +0000

    created full DAG with labels

--- DAGs.R
+++ DAGs.R
@@ -9,8 +9,8 @@ library(tidyverse)
 d <- read_xlsx("EFiT_Main data set_31OCT2019.xlsx") %>% 
   filter(partNum != c(3, 30))
 
-# Set the coordinates for the nodes in our DAG, this step is only for 
-coords <- data.frame(matrix(c("age",0,0,
+#This code can set the coordinates for the nodes in our DAG
+#coords <- data.frame(matrix(c("age",0,0,
                               "hearThresh",0,1,
                               "tinOnset",0,2,
                               "tinDur",0,3,
@@ -23,43 +23,45 @@ coords <- data.frame(matrix(c("age",0,0,
                               "anxiety",0.9,0.4,
                               "stress",0.8,0.4,
                               "EF",0,0), nrow=13, ncol=3, byrow=T))
-colnames(coords) <- c("name","x","y")
+#colnames(coords) <- c("name","x","y")
 
 #Create DAG for EF -> t models
 # Initialize the DAG. "y ~ x" means that x is a parent of y (y <- x)
-m1 <- dagify(EF ~ THI,
+EF_t_m1 <- dagify(
+            THI ~ T,
+            THI ~ EF,
+            C2 ~ C1,
+            T ~ C2,
+            EF ~ C3 + C4 + C5,
+            THI ~ C6 + C7,
+            EF ~ C8,
+            EF ~ C1,
              labels =
                c("EF" = "Executive \n function",
-                 "THI" = "Tinnitus \n severity"), 
-              latent = 
-               c("EF, THI"),
-              exposure = "EF",
+                 "THI" = "Tinnitus \n severity",
+                 "T" = "Onset",
+                 "TD" = "Duration",
+                 "C1" = "Age",
+                 "C2" = "Hearing \n thresholds",
+                 "C3" = "Depression",
+                 "C4" = "Anxiety",
+                 "C5" = "Stress",
+                 "C6" = "Rumination",
+                 "C7" = "Cognitive \n failures",
+                 "C8" = "Cognitive \n reserve"), 
+              exposure = "T",
               outcome = "THI") %>% 
   tidy_dagitty()
 
 
-
 # Plot the DAG
-m1 %>% ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+EF_t_m1 %>% ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_dag_edges() +
   geom_dag_point() +
   geom_dag_text() +
   scale_adjusted() +
-  geom_dag_label_repel(aes(label = label, fill = label),
-                       col = "white", show.legend = FALSE) 
-
- ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_dag_node(
-    color = "black",
-    alpha = 0.8
-  ) +
-  geom_dag_label_repel(aes(label = label),
-                       col = "white",
-                       label.size = .4,
-                       fill = "#20a486ff",
-                       alpha = 0.8,
-                       show.legend = FALSE,
-                       nudge_x = .7,
-                       nudge_y = .3
-  ) +
+  geom_dag_label_repel(aes(label = label, fill = "black"),
+                       col = "white", show.legend = FALSE) +
   theme_void()
+  
+
