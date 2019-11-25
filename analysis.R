9940d518f4fbd892ca6bd9aa41e61d418d7f6a91
Nathan A Clarke <n.clarke1986@gmail.com>
2019-11-25 12:31:08 +0000

    start analysis

--- analysis.R
+++ analysis.R
@@ -8,6 +8,46 @@ library(ggdag)
 library(readxl)
 library(tidyverse)
 
-d <-read_xlsx("EFiT_Main data set_31OCT2019.xlsx") # load data
+efit <- read_xlsx("EFiT_Main data set_05NOV2019.xlsx") %>% 
+  filter(!partNum == 3, !partNum ==30, !partNum == 79) #remove participants 3, 30 
+efit <- efit %>% rename(participant = partNum)
 
-skim(d)
+stroop <- read_csv("stroopTrimmed.csv")
+ruleSwitchCost <- read_csv("switchCostMeans.csv") 
+efit <- left_join(efit, ruleSwitchCost, by = "participant")#join switch cost data
+efit <- left_join(efit, stroop, by = "participant")#join stroop data
+
+#Descriptives
+#Create categorical variable (THI = 0 = none, THI <= 37 = mild, THI >= 38 = modsev)
+efit <- efit %>% 
+  mutate(
+    THI_cat = ifelse(
+      THI >= 38 ,"modsev", ifelse(
+        THI > 0 & THI <= 37, "mild", "none"
+      ))) #add a categorical variable with the groups split by THI scores
+efit %>% select(participant, THI, THI_cat)
+
+efit$THI_cat <- as.factor(efit$THI_cat) %>% ordered(levels = c("none", "mild", "modsev"))#factorise tinnitus groups
+
+#group_by tinnitus groups and count
+efit %>% 
+  group_by(THI_cat) %>% 
+  summarise(group_count = n()) %>% 
+  arrange(THI_cat)
+
+#create new RIF variable (proportion of correct respones and RIFdif (i.e. a measure of the effect)
+efit <- efit %>% 
+mutate(pRIF_RpPlus = (RIF_RpPlus/12) * 100,
+       pRIF_RpMinus = (RIF_RpMinus/12) * 100,
+       pRIF_Np = (RIF_Np/24) * 100,
+       RIF_Diff = pRIF_Np - pRIF_RpMinus)
+
+#summarise RIF scores between groups
+efit %>% 
+  group_by(THI_cat) %>% 
+  summarise(Diff = mean(RIF_Diff),
+            Plus = mean(pRIF_RpPlus),
+            Minus = mean(pRIF_RpMinus),
+            Np = mean(pRIF_Np))
+
+
