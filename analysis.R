0f86d2cc08cb962736e11a2a971f3a4d26788973
Nathan A Clarke <n.clarke1986@gmail.com>
2019-11-27 11:43:35 +0000

    began to add between group tests and effect size for measures

--- analysis.R
+++ analysis.R
@@ -1,7 +1,7 @@
 #EFiT data analysis - 25/11/2019
 
 #load packages and data
-library(dagitty)
+library(effectsize)
 library(skimr)
 library(bayestestR)
 library(ggdag)
@@ -25,9 +25,17 @@ efit <- efit %>%
       THI >= 38 ,"modsev", ifelse(
         THI > 0 & THI <= 37, "mild", "none"
       ))) #add a categorical variable with the groups split by THI scores
-efit %>% select(participant, THI, THI_cat)
+efit %>% select(participant, THI, THI_cat)#check it's worked
 
 efit$THI_cat <- as.factor(efit$THI_cat) %>% ordered(levels = c("none", "mild", "modsev"))#factorise tinnitus groups
+
+#filter by groups for statistical tests
+controls <- efit %>% 
+  filter(THI_cat == "none")
+mild_tin <- efit %>% 
+  filter(THI_cat == "mild")
+modsev_tin <- efit %>% 
+  filter(THI_cat == "modsev")
 
 #group_by tinnitus groups and count
 efit %>% 
@@ -35,7 +43,9 @@ efit %>%
   summarise(group_count = n()) %>% 
   arrange(THI_cat)
 
-#create new RIF variable (proportion of correct respones and RIFdif (i.e. a measure of the effect)
+###RIF 
+
+#create new RIF variable (proportion of correct respones and RIFdif (i.e. a measure of the RIF effect)
 efit <- efit %>% 
 mutate(pRIF_RpPlus = (RIF_RpPlus/12) * 100,
        pRIF_RpMinus = (RIF_RpMinus/12) * 100,
@@ -50,4 +60,24 @@ efit %>%
             Minus = mean(pRIF_RpMinus),
             Np = mean(pRIF_Np))
 
+#t.tests for RIF effect
+t.test(controls$RIF_Diff, modsev_tin$RIF_Diff, alternative = "greater",var.equal = F)#controls greater than modsev
+t.test(mild_tin$RIF_Diff, modsev_tin$RIF_Diff, alternative = "greater",var.equal = F)#mild greater than modsev
 
+#effect size for RIF effect
+cohens_d(controls$RIF_Diff, modsev_tin$RIF_Diff) %>% interpret_d(rules = "funder2019")
+cohens_d(modsev_tin$RIF_Diff, controls$RIF_Diff) %>% interpret_d(rules = "funder2019")
+
+###Number-letter sequencing
+#summarise NLS scores between groups
+efit %>% 
+  group_by(THI_cat) %>% 
+  summarise(numLetSeqMean = mean(numLetSeq))
+
+#t.tests for NLS scores
+t.test(controls$numLetSeq, modsev_tin$numLetSeq, alternative = "greater",var.equal = F)#controls greater than modsev
+t.test(mild_tin$numLetSeq, modsev_tin$numLetSeq, alternative = "greater",var.equal = F)#mild greater than modsev
+
+#effect size for NLS scores
+cohens_d(controls$numLetSeq, modsev_tin$numLetSeq) %>% interpret_d(rules = "funder2019")
+cohens_d(modsev_tin$numLetSeq, mild_tin$numLetSeq) %>% interpret_d(rules = "funder2019")
